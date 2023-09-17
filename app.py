569093ea44d8ea486141c03c6b82b00ab03c679e
caiquemiranda <caiquemirandas@gmail.com>
2023-09-17 13:53:27 -0300

    model_v00

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Esse é meu '
+def index():
+    return render_template('index.html')
 
-# run the app
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
